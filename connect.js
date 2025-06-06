19e60c669d6cf27a09d2a445369a70024114b989
aidandrought <155926412+aidandrought@users.noreply.github.com>
2025-06-05 23:59:07 -0700

    Update connect.js
    Better error logging for DB connection

--- connect.js
+++ connect.js
@@ -2,8 +2,17 @@ const mongoose = require("mongoose");
 
 const connectionString = "mongodb+srv://Team4:1234@cluster0.pdc2xzl.mongodb.net/TM-T4?retryWrites=true&w=majority";
 
-const connectDB = () => {
-	return mongoose.connect(connectionString);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Connected to MongoDB Atlas.");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
